c4394c421fd292938dd3a2024b676ce916680494
Zack Miller <126508376+ZmCoder2@users.noreply.github.com>
2023-05-22 18:22:58 -0400

    I may have fixed the problem lets see

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlRoutes = require("./routes/html-routes")
 const apiRoutes = require('./routes/api-routes')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
